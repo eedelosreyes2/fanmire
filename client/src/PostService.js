9ba437b47c592184aef643b2f0ec42754248446d
eedelosreyes2 <eedelosreyes2@dons.usfca.edu>
2020-03-17 15:28:22 -0700

    set up client

---
+++ client/src/PostService.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const url = 'http://localhost:5000/api/posts/'; // change to proxy later
+
+class PostService {
+  // Get Posts
+  static getPosts() {
+    return new Promise((resolve, reject) => {
+      axios.get(url).then((res) => {
+          const data = res.data;
+          resolve(
+            data.map(post => ({
+              ...post,
+              createdAt: new Date(post.createdAt)
+            }))
+          );
+        })
+        .catch((err) => {
+          reject(err);
+        })
+    });
+  }
+
+  // Create Post
+  static insertPost(text) {
+    return axios.post(url, {
+      text
+    });
+  }
+
+  // Delete Post
+  static deletePost(id) {
+    return axios.delete(`${url}${id}`);
+  }
+}
+
+export default PostService;
