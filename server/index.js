7808afca1c7c877f80f6ccf807f76493896a3ecd
eedelosreyes2 <eedelosreyes2@dons.usfca.edu>
2020-04-27 21:30:09 -0700

    fixed refactoring error

--- server/index.js
+++ server/index.js
@@ -1,35 +1,17 @@
-/* Revtrieve Environment Variables from .env */
-const path = require('path')
-require('dotenv').config({
-  path: path.resolve(__dirname, '../../.env')
-});
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const app = express();
-var fs = require("fs")
-var https = require("https");
+const server = express();
 
 // Middleware
-app.use(bodyParser.json());
-app.use(cors());
+server.use(bodyParser.json());
+server.use(cors());
 
 const apiRoutes = require('./routes/api');
 
-app.use('/api', apiRoutes);
+server.use('/api', apiRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-// https.createServer({
-//     key: process.env.SERVER_KEY,
-//     cert: process.env.SERVER_CERT
-//   }, server)
-//   .listen(PORT, function() {
-//     console.log('Example app listening on port 5000! Go to https://localhost:5000/')
-//   })
-
-//server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,10 +1,11 @@
 const {
   Router
 } = require('express');
-
+const postsRouter = require('./twitter');
 const identityRouter = require('./identities');
 
 const router = Router();
+router.use('/posts', postsRouter);
 router.use('/identity', identityRouter);
 
 module.exports = router;
