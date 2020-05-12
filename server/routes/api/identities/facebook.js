2ac84bb8ff81f84131ecdda88080258717e02b3f
ankitakhatri <ankitakhatri58@gmail.com>
2020-05-12 10:48:53 -0700

    adding comments and documentation

--- server/routes/api/identities/facebook.js
+++ server/routes/api/identities/facebook.js
@@ -1,9 +1,10 @@
-/* Revtrieve Environment Variables from .env */
+/* Retrieve Environment Variables from .env */
 const path = require('path')
 require('dotenv').config({
   path: path.resolve(__dirname, '../../../../.env')
 });
 
+//content schema
 const Content = require('../../../models/content_schema');
 
 const {
@@ -13,26 +14,27 @@ const axios = require('axios');
 
 const router = new Router();
 
+//required for parsing the json response and putting in the database
 const mongodb = require('mongodb');
 const mongoose = require('mongoose');
 
-//TODO: make a parse function
+//parse function to take JSON response and put it in content schema format
 async function parse(data) {
 
   var parsed_posts = [];
   var i;
 
   //iterate through posts
-  //console.log(data.accounts.data[0].posts.data.length)
   for (i = 0; i < data.accounts.data[0].posts.data.length; i++) {
+    //get each individual post
     var post = data.accounts.data[0].posts.data[i];
-
-    //console.log(post)
 
     var images = [];
 
+    //grab images from the post
     images.push(post.picture);
 
+    //parse the rest of the fields of the post ie message, created time, name
     var parsed_post = new Content({
       _id: new mongoose.Types.ObjectId(),
       social_media: 'Facebook',
@@ -41,12 +43,12 @@ async function parse(data) {
       content_images: images,
       created_date: Date.parse(post.created_time)
     });
+    //add parsed_post to array of parsed posts
     parsed_posts.push(parsed_post);
   }
-  //do the same for the photos
+  //return array of parsed posts
   return parsed_posts;
 }
-
 
 router.post('/', async (req, res) => {
   const {
@@ -55,7 +57,9 @@ router.post('/', async (req, res) => {
     }
   } = req;
 
+  //call to facebook graph api, include the fields you want to pull
   const URL = 'https://graph.facebook.com/v6.0/me';
+  //get the fan pages linked to the influencer's account, and pull the posts from that page
   const userFieldSet = 'accounts{name,posts{message,picture,created_time}}';
 
   const options = {
@@ -65,29 +69,26 @@ router.post('/', async (req, res) => {
     }
   };
 
+  //load posts collection
   const posts = await loadPostsCollection();
 
+  //get the data from facebook graph api
   const {
     data
   } = await axios.get(URL, options)
 
-  //console.log(data);
-
-  //Make into content format
+  //parse data into content schema format
   const parsed_data = await parse(data)
 
   console.log(parsed_data);
 
   res.json(parsed_data);
 
-  // Add Tweets to MongoDB
-  // (async () => {
-  //   posts.insertMany(parsed_data);
-  // })();
-   await posts.insertMany(parsed_data);
-   //console.log(posts);
+  //insert the parsed data into the database
+  await posts.insertMany(parsed_data);
 });
 
+//function to load posts collection and post to database
 async function loadPostsCollection() {
   const client = await mongodb.MongoClient.connect(process.env.MONGODB_STRING, {
     useNewUrlParser: true,
